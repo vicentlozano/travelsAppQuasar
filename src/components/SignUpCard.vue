c2086f18863ed339194c9914ad6472e5695aec97
vicentlozano <lozanodroid@gmail.com>
2025-04-22 18:00:32 +0200

    fix

--- src/components/SignUpCard.vue
+++ src/components/SignUpCard.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="register-card">
     <h5 class="title">{{ text[step] }}</h5>
-    <q-form @submit.prevent class="q-gutter-md custom">
+    <q-form @submit.prevent class=" custom">
       <section v-if="step === 0" class="inputs-name">
         <q-input
           outlined
@@ -34,47 +34,26 @@
       </section>
       <section v-if="step === 1" class="inputs-name">
         <q-input
-          outlined
-          label="Day"
-          stack-label
-          v-model="day"
-          class="input"
           filled
-          type="day"
-          @keyup.enter="prompt = false"
-          lazy-rules
+          v-model="dateStamp"
+          mask="date"
+          :rules="['date']"
           dense
           bg-color="white"
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-        />
-        <q-input
-          outlined
-          label="Month"
-          stack-label
           class="input"
-          v-model="month"
-          filled
-          type="month"
-          @keyup.enter="prompt = false"
-          lazy-rules
-          dense
-          bg-color="white"
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-        />
-        <q-input
-          outlined
-          label="Year"
-          stack-label
-          class="input"
-          v-model="year"
-          filled
-          type="year"
-          @keyup.enter="prompt = false"
-          lazy-rules
-          dense
-          bg-color="white"
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-        />
+        >
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <q-date v-model="dateStamp" :locale="myLocale">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
         <q-select
           stack-label
           label="Gender"
@@ -85,7 +64,7 @@
           bg-color="white"
           v-model="gender"
           :options="optionsGender"
-          style="width: 250px"
+          class="input"
         />
       </section>
       <section v-if="step === 2" class="inputs-email">
@@ -152,7 +131,7 @@
       <q-btn class="button right" rounded label="Next" @click="step++" />
     </section>
     <section v-else class="buttons">
-      <q-btn class="button right" rounded label="Next" type="button"  @click="signUpAction"/>
+      <q-btn class="button right" rounded label="Next" type="button" @click="signUpAction" />
     </section>
   </section>
 </template>
@@ -163,13 +142,12 @@ import { signUp } from '../utils/api/post.js'
 import md5 from 'md5'
 import { useRouter } from 'vue-router'
 import { notifyError, notifySuccess } from 'src/utils/utils.js'
+import moment from 'moment'
 const router = useRouter()
 const step = ref(0)
 const name = ref('')
 const lastName = ref('')
-const day = ref(null)
-const month = ref(null)
-const year = ref(null)
+const dateStamp = ref(moment().format('YYYY/MM/DD'))
 const gender = ref('')
 const optionsGender = ['Male', 'Female', 'Rather not say', 'Other']
 const text = [
@@ -184,7 +162,7 @@ const repeatPassword = ref('')
 
 const isPwd = ref(true)
 const isPwd2 = ref(true)
-
+console.log(dateStamp.value)
 const signUpAction = async () => {
   const user = {
     name: name.value,
@@ -222,49 +200,38 @@ const signUpAction = async () => {
   background-color: transparent;
 }
 .register-card {
-  width: 570px;
-  height: 500px;
+  width: 450px;
+  height:500px;
   display: grid;
   gap: 1rem;
-  grid-template-rows: 1fr 3fr;
+  grid-template-rows: 1fr 4fr 1fr;
   text-align: center;
   padding: 2rem 2rem 0 2rem;
-  background-color: $gray-accent;
+  background-color: #42464e83;
   border-radius: 25px;
   place-items: center;
-  justify-content: center;
+  align-content: center;
 }
 .title {
   color: white;
   font-weight: bolder;
   letter-spacing: 0.05em;
 }
-.inputs-name {
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  gap: 0.7rem;
-  width: 100%;
-  align-self: center;
-  place-items: center;
-  margin: 0;
-}
+
 .buttons {
-  display: grid;
-  grid-template-columns: 130px 130px;
+  display: flex;
   gap: 1rem;
-  padding: 0 1rem;
   width: 100%;
   justify-content: space-between;
   padding-bottom: 0.6rem;
 }
 .only-button {
-  display: grid;
+  display: flex;
   grid-template-columns: 1fr;
   gap: 1rem;
-  width: 30%;
-  align-items: right;
-  justify-self: right;
+  width: 100%;
   padding-bottom: 0.6rem;
+  justify-content: right;
 }
 .button {
   width: 8rem;
@@ -274,20 +241,40 @@ const signUpAction = async () => {
   color: white;
 }
 .input {
-  min-width: 300px;
+  width: 250px;
+}
+.date {
+  width: 8rem;
 }
 .inputs-email {
   display: grid;
   grid-template-rows: 1fr 1fr 1fr;
-  gap: 0.7rem;
+  gap: 0.1rem;
   width: 100%;
+  height: fit-content;
   align-self: center;
   place-items: center;
+}
+.inputs-name {
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  gap: 0.7rem;
+  width: 100%;
+  height: fit-content;
+  justify-content: center;
+  margin: 0;
 }
 .custom {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.date-section {
   display: grid;
-  grid-template-rows: 1fr 0.2fr;
+  grid-template-columns: 1fr 1fr 1fr;
+  width: 100%;
+  gap: 1.5rem;
 }
 </style>
