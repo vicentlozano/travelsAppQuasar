26b0ac2036f6dff7550111e336a29088dbb9f4d4
vicentlozano <lozanodroid@gmail.com>
2025-05-08 00:16:57 +0200

    mqtt

--- src/components/chat/ChatDialog.vue
+++ src/components/chat/ChatDialog.vue
@@ -1,12 +1,15 @@
 <template>
   <q-scroll-area
+    ref="chatScroll"
     :thumb-style="thumbStyle"
     :bar-style="barStyle"
     style="height: 100%; width: 100%"
     id="scroll-area-with-virtual-scroll-1"
   >
     <div class="chat-dialog">
-      <q-chat-message :label="messages[0]?.date? moment(messages[0]?.date).format('DD MMMM'): ''" />
+      <q-chat-message
+        :label="messages[0]?.date ? moment(messages[0]?.date).format('DD MMMM') : ''"
+      />
       <q-chat-message
         v-for="message in messages"
         :key="message.message"
@@ -50,40 +53,77 @@
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, watch, inject,nextTick  } from 'vue'
 import { getMessages } from 'src/utils/api/get'
 import { useUserStore } from 'src/stores/user'
 import moment from 'moment'
 import { notifyError } from 'src/utils/utilsNotify'
+
 //props&emits
 const props = defineProps({
   contactChat: Object,
 })
+
 //data
+const mqtt = inject('appGlobal/mqtt')
 const user = useUserStore()
 const messages = ref([])
 const contactAvatar = ref(null)
 const constactIsWritting = ref(true)
+const chatScroll = ref(null)
+
 //computed
 watch(
   () => props.contactChat,
-  async (newVal) => {
+  async (oldVal, newVal) => {
     if (newVal) {
       try {
+        let newTopic = user.userId + props.contactChat.id
+        let oldTopic = user.userId + oldVal.id
+
         messages.value = await getMessages({ userId: user.userId, friendId: newVal.id })
         messages.value = messages.value.data.data
         contactAvatar.value = newVal?.avatar.length > 0 ? newVal?.avatar : ''
+        mqtt.unSubscribe(`TRAVELS/UPDATES/${oldTopic}`)
+
+        mqtt.subscribe(`TRAVELS/UPDATES/${newTopic}`, (message) => {
+          messages.value.push(JSON.parse(message))
+        })
+        scrollToBottom(true)
       } catch (error) {
         notifyError(error)
       }
     }
   },
 )
+watch(messages, () => {
+  scrollToBottom()
+})
+
+//methods
+
+function scrollToBottom() {
+  nextTick(() => {
+    const scrollEl = chatScroll.value?.getScrollTarget?.()
+    if (scrollEl) {
+      scrollEl.scrollTo({
+        top: scrollEl.scrollHeight,
+        behavior: 'smooth' // <-- ¡Este es el scroll suave real!
+      })
+    }
+  })
+}
+//hooks
 onMounted(async () => {
   try {
     messages.value = await getMessages({ userId: user.userId, friendId: props.contactChat.id })
     messages.value = messages.value.data.data
     contactAvatar.value = props.contactChat?.avatar.length > 0 ? props.contactChat?.avatar : ''
+    let topic = user.userId + props.contactChat.id
+    mqtt.subscribe(`TRAVELS/UPDATES/${topic}`, (message) => {
+      messages.value.push(JSON.parse(message))
+    })
+    scrollToBottom(true)
   } catch (error) {
     notifyError(error)
   }
