39e0afa6d2d62f08313eb87732f713fb03616157
vicentlozano <lozanodroid@gmail.com>
2025-05-29 19:09:07 +0200

    fix

--- src/boot/mqtt.js
+++ src/boot/mqtt.js
@@ -2,7 +2,9 @@ import { defineBoot } from '#q-app/wrappers'
 import MQTT from 'src/utils/mqttManager'
 
 export default defineBoot(({ app }) => {
-  const ip = 'travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net'
+  const ip = 'localhost'
+  //  const ip = 'localhost'
+
   const port = '9001'
 
   if (ip && port) {
