498aa8cb38c126ce65cbe437ddb4ba5089e7c0b3
vicentlozano <lozanodroid@gmail.com>
2025-05-29 20:09:23 +0200

    fix

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -11,7 +11,7 @@ import { LocalStorage } from 'quasar'
 const api = axios.create({
   baseURL: '/api/wsTravels/',
 
-  // baseURL: 'https://travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net/wsTravels/',
+  //baseURL: 'https://travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net/wsTravels/',
   //travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net
   timeout: 6000,
   withCredentials: true, // <-- AIXÒ ÉS IMPORTANT
