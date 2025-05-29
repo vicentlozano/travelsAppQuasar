c01660054d9168099dddb84edc3195d40d4ff4ce
vicentlozano <lozanodroid@gmail.com>
2025-05-29 21:08:20 +0200

    fix

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -9,7 +9,7 @@ import { LocalStorage } from 'quasar'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: 'travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net/wsTravels/',
+  baseURL: 'https://travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net/wsTravels/',
 
   //baseURL: 'https://travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net/wsTravels/',
   //travlesbackend-bmg4gtc5a0d9bbbd.canadacentral-01.azurewebsites.net
